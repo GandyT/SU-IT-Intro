d8ddb75db000658f1d45aab490d5c501ce610b45
Caden Khuu <117611429+cadenk0@users.noreply.github.com>
2023-09-25 12:57:26 -0400

    Update update.js

--- expressjs-intro/request-practice/update.js
+++ expressjs-intro/request-practice/update.js
@@ -27,20 +27,24 @@ const run = async () => {
 }
 
 const updateData = async () => {
-    /* FETCH DATA FROM SERVER + PRINT IT OUT */
-    /* UPDATE DATA IN SERVER WITH WHATEVER VALUE YOU WANT */
-    /* FETCH DATA AGAIN AND PRINT IT OUT */
-    const data = await fetch(getUrl)
-    const response = await data.json()
-    console.log(response)
-    await fetch(postUrl, {
-        method : "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({ value: "New and updated value"})
-    }) 
-    const newData = await fetch(getUrl)
-    const newResponse = await newData.json()
-    console.log(newResponse)
+    try {
+        const getData = await fetch(getUrl);
+        const initialResponse = await getData.json();
+        console.log("Initial Data:", initialResponse);
+
+        const newValue = "New and updated value";
+        await fetch(postUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ value: newValue }),
+        });
+
+        const updatedData = await fetch(getUrl);
+        const updatedResponse = await updatedData.json();
+        console.log("Updated Data:", updatedResponse);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-updateData()
+updateData();
