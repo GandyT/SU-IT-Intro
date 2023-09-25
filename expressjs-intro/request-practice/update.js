918ac3a787844eda28cc58e524a82f12ae5d76fc
k4limul <kalimul.kaif@stuypulse.com>
2023-09-25 14:25:35 -0400

    Added updateData() to print out initial data from server, then replace it and print newData

--- expressjs-intro/request-practice/update.js
+++ expressjs-intro/request-practice/update.js
@@ -1,46 +1,58 @@
 /* nodejs server does not have fetch. Must be installed as a library */
+const { request } = require("express")
 const fetch = require("node-fetch")
 
 const getUrl = "http://localhost:8000/api/getdata"
 const postUrl = "http://localhost:8000/api/updatedata"
 
 /* REFERENCE FUNCTION. do not run this, just look at it. */
-const run = async () => {
-    const data = await fetch(getUrl)
-    const response = await data.json()
+// const run = async () => {
+//     const data = await fetch(getUrl)
+//     const response = await data.json()
 
-    console.log(response)
+//     console.log(response)
 
-    await fetch(
-        postUrl, 
-        { 
-            method: "POST", 
-            headers: { "Content-Type": "application/json"}, 
-            body: JSON.stringify({ value: "New and updated value"})
-        }
-    )
+//     await fetch(
+//         postUrl, 
+//         { 
+//             method: "POST", 
+//             headers: { "Content-Type": "application/json"}, 
+//             body: JSON.stringify({ value: "New and updated value"})
+//         }
+//     )
 
-    const newData = await fetch(getUrl)
-    const newResponse = await newData.json()
+//     const newData = await fetch(getUrl)
+//     const newResponse = await newData.json()
 
-    console.log(newResponse)
-}
+//     console.log(newResponse)
+// }
 
-const updateData = async () => {
     /* FETCH DATA FROM SERVER + PRINT IT OUT */
     /* UPDATE DATA IN SERVER WITH WHATEVER VALUE YOU WANT */
     /* FETCH DATA AGAIN AND PRINT IT OUT */
-    const data = await fetch(getUrl)
-    const response = await data.json()
-    console.log(response)
-    await fetch(postUrl, {
-        method : "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({ value: "New and updated value"})
-    }) 
-    const newData = await fetch(getUrl)
-    const newResponse = await newData.json()
-    console.log(newResponse)
-}
 
-updateData()
+const updateData = async() => {
+    const receivedData = await fetch(getUrl); // fetches data from server
+    const firstData = await receivedData.json(); // converts to json as soon as data is fetched
+    console.log(firstData); // prints receivedData
+ 
+    let newData = {
+        value: {
+            name: "kalimul",
+            age: 15 ,
+            grade: "sophomore" 
+        }
+}   
+
+    await fetch(postUrl, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newData)
+        }
+    )
+
+    const responseData = await fetch(getUrl);
+    const rjson = await responseData.json();
+    console.log(rjson);
+}
+updateData();
