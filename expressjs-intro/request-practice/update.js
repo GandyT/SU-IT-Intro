c1da9dcb61a78d089c5d1644ea32abf3218fbf23
Tony Chen <tchen51@stuy.edu>
2023-09-25 12:43:28 -0400

    complete updateData

--- expressjs-intro/request-practice/update.js
+++ expressjs-intro/request-practice/update.js
@@ -30,4 +30,17 @@ const updateData = async () => {
     /* FETCH DATA FROM SERVER + PRINT IT OUT */
     /* UPDATE DATA IN SERVER WITH WHATEVER VALUE YOU WANT */
     /* FETCH DATA AGAIN AND PRINT IT OUT */
+    const data = await fetch(getUrl)
+    const response = await data.json()
+    console.log(response)
+    await fetch(postUrl, {
+        method : "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ value: "New and updated value"})
+    }) 
+    const newData = await fetch(getUrl)
+    const newResponse = await newData.json()
+    console.log(newResponse)
 }
+
+updateData()
