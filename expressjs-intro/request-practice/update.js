2ca5cb99df9ccd6bd7cc6f145e7659aa35059130
Rahuldeb5 <110701518+Rahuldeb5@users.noreply.github.com>
2023-09-30 08:07:26 -0400

    Update update.js

--- expressjs-intro/request-practice/update.js
+++ expressjs-intro/request-practice/update.js
@@ -6,11 +6,13 @@ const postUrl = "http://localhost:8000/api/updatedata"
 
 /* REFERENCE FUNCTION. do not run this, just look at it. */
 const run = async () => {
+    /* FETCHES THE DATA */
     const data = await fetch(getUrl)
     const response = await data.json()
 
-    console.log(response)
-
+    console.log(response) // LOGS THE RESPONSE
+    
+    // CREATE A POST REQUEST updating the data
     await fetch(
         postUrl, 
         { 
@@ -19,28 +21,34 @@ const run = async () => {
             body: JSON.stringify({ value: "New and updated value"})
         }
     )
-
+    
+    // create a get request to see the updated data
     const newData = await fetch(getUrl)
     const newResponse = await newData.json()
-
+    
+    // log the updated data into the console
     console.log(newResponse)
 }
 
 const updateData = async () => {
     /* FETCH DATA FROM SERVER + PRINT IT OUT */
-    /* UPDATE DATA IN SERVER WITH WHATEVER VALUE YOU WANT */
-    /* FETCH DATA AGAIN AND PRINT IT OUT */
     const data = await fetch(getUrl)
-    const response = await data.json()
-    console.log(response)
-    await fetch(postUrl, {
-        method : "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({ value: "New and updated value"})
-    }) 
+    const json = await data.json()
+    console.log(json)
+    
+    /* UPDATE DATA IN SERVER WITH WHATEVER VALUE YOU WANT */
+    const update =  {
+        method: "POST", 
+        headers: { "Content-Type": "application/json"}, 
+        body: JSON.stringify({ value: "This is my new and final data! I love JS!"})
+    }
+
+    await fetch(postUrl, update)
+    
+    /* FETCH DATA AGAIN AND PRINT IT OUT */
     const newData = await fetch(getUrl)
-    const newResponse = await newData.json()
-    console.log(newResponse)
+    const newJSON = await newData.json()
+    console.log(newJSON)
 }
 
 updateData()
